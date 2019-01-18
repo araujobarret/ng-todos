931ec4c4412efb90a98149a455b4514757933f2b
Carlos Alberto de Araujo Barreto <araujobarret@gmail.com>
2019-01-18 15:25:37 -0200

    Login UI ready. Maybe can be a good idea to create the login module with each material UI component import there istead of importing on app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+// Most used UI Components
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { routes } from './app.routes';
@@ -26,9 +33,14 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
     DashboardModule,
     AppRoutingModule,
-    ServicesModule
+    ServicesModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   bootstrap: [AppComponent]
 })
